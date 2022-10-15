2faa638e9372f2cb0a1961daa474198d430c3718
Humberto24 <humberto.vaco_24@hotmail.com>
2022-10-15 14:35:49 -0600

    small change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and integrated with travis😂😂😂.
+          Edit <code>src/App.js</code> and integrated with traviss😂😂😂.
         </p>
         <a
           className="App-link"
